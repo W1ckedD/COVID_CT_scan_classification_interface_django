a02c74970e26bfe443a35fc5e20a7823cad12283
w1ckedd <alirezachoobineh@ymail.com>
2022-01-25 11:05:52 +0330

    added samples model

--- core/samples/models.py
+++ core/samples/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Sample(models.Model):
+  account = models.ForeignKey(User, related_name='samples', on_delete=models.CASCADE)
+  name = models.CharField(max_length=100)
+  image = models.ImageField(upload_to='photos/%Y/%m/%d/')
+  result = models.CharField(
+    max_length=50,
+    choices=[
+      ('POS', 'مبتلا به گوید 19'),
+      ('NEG', 'عدم ابتلا به کوید 19'),
+      ('TBD', 'نا مشخص')
+    ],
+    default='TBD'
+  )
+  probability = models.FloatField(blank=True)
+  visible_to_public = models.BooleanField(default=True)
+  visible_to_users = models.BooleanField(default=True)
+
+  def __str__(self):
+    return f'{self.account}, {self.name}'
