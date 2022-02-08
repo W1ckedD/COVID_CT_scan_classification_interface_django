8810e7467030a03e31033dfa872952d5312f8b69
w1ckedd <alirezachoobineh@ymail.com>
2022-02-08 08:40:50 +0330

    added Log model

--- core/admin_panel/models.py
+++ core/admin_panel/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Log(models.Model):
+  account = models.ForeignKey(User, on_delete=models.CASCADE, related_name='logs')
+  msg = models.CharField(max_length=150)
+  action_type = models.CharField(max_length=50, choices=[
+    ('AUTH', 'Authentication action type'),
+    ('CREATE', 'create object in the db'),
+    ('READ', 'read object in the db'),
+    ('UPDATE', 'update object in the db'),
+    ('DELETE', 'delete object in the db'),
+    ('PREDICT', 'use the COVID prediction api'),
+  ])
+  created_at = models.DateTimeField(auto_now_add=True)
