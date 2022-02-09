fd1dc65ea0e928c1875239c49a98b63d89ced2d2
w1ckedd <alirezachoobineh@ymail.com>
2022-02-09 10:10:57 +0330

    added remaining log entries

--- core/samples/views.py
+++ core/samples/views.py
@@ -1,12 +1,12 @@
 import os
-from unittest import result
 from django.utils import timezone
 from django.shortcuts import redirect, render, get_object_or_404
 from django.views import View
 from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
-from django.db.models import F, Q, functions, Value
+from django.db.models import Q, functions, Value
 from .models import Sample
+from admin_panel.models import Log
 
 from tensorflow.keras.models import load_model
 from utils.preprocess import preprocess_img
@@ -38,6 +38,14 @@ class SamplesView(View):
 
     if request.user.is_authenticated:
       samples = samples.filter(visible_to_users=True).order_by('-issued_at')
+      
+      #Log
+      log = Log.objects.create(
+        account=request.user,
+        action_type='READ',
+        msg='خواندن نمونه های قابل مشاهده.'
+      )
+      
       return render(request, 'samples/samples.html', context={'samples': samples})
     else:
       samples = samples.filter(visible_to_public=True).order_by('-issued_at')
@@ -47,6 +55,14 @@ class MySamplesView(View):
   @login_required('accounts_login')
   def get(self, request, *args, **kwargs):
     samples = Sample.objects.filter(account=request.user).order_by('-issued_at')
+    
+    #Log
+    log = Log.objects.create(
+      account=request.user,
+      action_type='READ',
+      msg='خواندن نمونه های مربوط به خود کاربر.'
+    )
+    
     return render(request, 'samples/samples.html', context={'samples': samples})
 
 
@@ -77,11 +93,22 @@ class AddSampleView(View):
       sample.visible_to_users = False
       sample.visible_to_public = False
     sample.save()
+    #Log
+    log = Log.objects.create(
+      account=request.user,
+      action_type='CREATE',
+      msg='افزودن نمونه جدید.'
+    )
     return redirect('samples_my-samples')
 
 class SampleDetailsView(View):
   def get(self, request, *args, **kwargs):
     sample = get_object_or_404(Sample, id=kwargs.get('id'))
+    log = Log.objects.create(
+      account=request.user,
+      action_type='READ',
+      msg=f'خواندن نمونه {sample}.'
+    )
     return render(request, 'samples/my-sample-details.html', context={'sample': sample})
 
   @login_required('accounts_login')
@@ -91,6 +118,12 @@ class SampleDetailsView(View):
     sample_id = kwargs.get('id')
     sample = get_object_or_404(Sample, id=sample_id)
     if request.user != sample.account:
+      log = Log.objects.create(
+        account=request.user,
+        action_type='UPDATE',
+        msg=f'تغییر ناموفق نمونه {sample} به دلیل عدم مالکیت.',
+        success=False
+      )
       messages.error(request, 'شما دسترسی لازم جهت انجام این عمل را ندارید.')
       return redirect('samples_my-sample-details', sample_id)
 
@@ -105,6 +138,11 @@ class SampleDetailsView(View):
       sample.visible_to_users = False
       sample.visible_to_public = False
     sample.save()
+    log = Log.objects.create(
+      account=request.user,
+      action_type='UPDATE',
+      msg=f'تغییر نمونه {sample}.'
+    )
     messages.success(request, 'تغییرات با موفقیت ثبت شد.')
     return redirect('samples_my-sample-details', sample_id)
 
@@ -116,6 +154,12 @@ class PredictSampleView(View):
     sample_id = request.POST.get('sample_id')
     sample = get_object_or_404(Sample, id=sample_id)
     if request.user != sample.account:
+      log = Log.objects.create(
+        account=request.user,
+        action_type='PREDICT',
+        msg=f'محاسبه ناموفق احتمال نمونه {sample} به دلیل عدم مالکیت.',
+        success=False
+      )
       messages.error(request, 'شما دسترسی لازم جهت انجام این عمل را ندارید.')
       return redirect('samples_my-sample-details', sample_id)
     IMG_DIR = os.path.join(os.getcwd(), 'media', str(sample.image).split('/')[2])
@@ -132,6 +176,11 @@ class PredictSampleView(View):
     sample.probability = probability * 100
     sample.predicted_at = timezone.now()
     sample.save()
+    log = Log.objects.create(
+      account=request.user,
+      action_type='PREDICT',
+      msg=f'محاسبه احتمال نمونه {sample}.'
+    )
     return redirect('samples_my-sample-details', sample_id)
 
 class EditSampleNameView(View):
@@ -140,6 +189,12 @@ class EditSampleNameView(View):
     sample_id = request.POST.get('sample_id')
     sample = get_object_or_404(Sample, id=sample_id)
     if request.user != sample.account:
+      log = Log.objects.create(
+        account=request.user,
+        action_type='UPDATE',
+        msg=f'تغییر ناموفق نام نمونه {sample} به دلیل عدم مالکیت.',
+        success=False
+      )
       messages.error(request, 'شما دسترسی لازم جهت انجام این عمل را ندارید.')
       return redirect('samples_my-sample-details', sample_id)
     name = request.POST.get('name')
@@ -149,6 +204,11 @@ class EditSampleNameView(View):
 
     sample.name = name
     sample.save()
+    log = Log.objects.create(
+      account=request.user,
+      action_type='UPDATE',
+      msg=f'تغییر نام نمونه {sample}.'
+    )
     messages.success(request, 'تغییرات با موفقیت ثبت شد.')
     return redirect('samples_my-sample-details', sample_id)
 
@@ -159,10 +219,21 @@ class DeleteSampleView(View):
     sample_id = request.POST.get('sample_id')
     sample = get_object_or_404(Sample, id=sample_id)
     if request.user != sample.account:
+      log = Log.objects.create(
+        account=request.user,
+        action_type='DELETE',
+        msg=f'حذف ناموفق نمونه {sample} به دلیل عدم مالکیت.',
+        success=False
+      )
       messages.error(request, 'شما دسترسی لازم جهت انجام این عمل را ندارید.')
       return redirect('samples_my-sample-details', sample_id)
     IMG_DIR = os.path.join(os.getcwd(), 'media', str(sample.image).split('/')[2])
     fs = FileSystemStorage()
     fs.delete(IMG_DIR)
+    log = Log.objects.create(
+      account=request.user,
+      action_type='DELETE',
+      msg=f'حذف نمونه {sample}.'
+    )
     sample.delete()
     return redirect('samples_my-samples')
