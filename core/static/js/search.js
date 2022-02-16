964ce6c728c3844ee4025ea0e5fe09cea26c787d
w1ckedd <alirezachoobineh@ymail.com>
2022-02-16 10:01:08 +0330

    added vue for search forms

--- core/static/js/search.js
+++ core/static/js/search.js
@@ -1,55 +1,41 @@
 const searchContainer = document.querySelector('#search');
 const btnSearch = document.querySelector('#btnSearch');
 
-const nameInput = document.querySelector('#name');
-const usernameInput = document.querySelector('#username');
-const ownerNameInput = document.querySelector('#owner_name');
-const posCheckbox = document.querySelector('#pos');
-const negCheckbox = document.querySelector('#neg');
-const tbdCheckbox = document.querySelector('#tbd');
-
 btnSearch.addEventListener('click', function () {
   searchContainer.classList.toggle('show');
 });
 
-const { name, username, owner_name, pos, neg, tbd } = Qs.parse(
-  location.search,
-  {
-    ignoreQueryPrefix: true,
+new Vue({
+  delimiters: ["[[", "]]"],
+  el: '#search-vue',
+  data() {
+    return {
+      show: true,
+      name: '',
+      username: '',
+      owner_name: '',
+      pos: false,
+      neg: false,
+      tbd: false,
+    };
+  },
+  created() {
+    this.prepopulate();
+  },
+  methods: {
+    prepopulate() {
+      const { name, username, owner_name, pos, neg, tbd } = Qs.parse(
+        location.search,
+        {
+          ignoreQueryPrefix: true,
+        }
+      );
+      this.name = name;
+      this.username = username;
+      this.owner_name = owner_name;
+      this.pos = pos === 'on';
+      this.neg = neg === 'on';
+      this.tbd = tbd === 'on';
+    },
   }
-);
-
-function prepopulateInputs() {
-  if (name) {
-    nameInput.value = name;
-  } else {
-    nameInput.value = '';
-  }
-  if (username) {
-    usernameInput.value = username;
-  } else {
-    usernameInput.value = '';
-  }
-  if (owner_name) {
-    ownerNameInput.value = owner_name;
-  } else {
-    ownerNameInput.value = '';
-  }
-  if (pos) {
-    posCheckbox.checked = true;
-  } else {
-    posCheckbox.checked = false;
-  }
-  if (neg) {
-    negCheckbox.checked = true;
-  } else {
-    negCheckbox.checked = false;
-  }
-  if (tbd) {
-    tbdCheckbox.checked = true;
-  } else {
-    tbdCheckbox.checked = false;
-  }
-}
-
-window.addEventListener('load', prepopulateInputs);
+});
